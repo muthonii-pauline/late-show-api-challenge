c93acf5ca3987dc3a159eda1af93fef1240f48fe
Muthonii <kinyanjuipauline69@gmail.com>
2025-06-23 22:59:02 +0300

    starts

--- server/controllers/guest_controller.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from ..models.guest import Guest
-
-guest_bp = Blueprint('guests', __name__)
-
-@guest_bp.route('/guests', methods=['GET'])
-def get_guests():
-    guests = Guest.query.all()
-    return jsonify([{"id": g.id, "name": g.name, "occupation": g.occupation} for g in guests])
